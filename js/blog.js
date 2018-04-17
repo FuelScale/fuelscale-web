093b06f5e5740ae33a120ddfa88b2d8466eeb8bc
shrey-winning <shrey.sharma@winningtechs.com>
2018-04-17 17:13:21 +0530

    Content updation

--- js/blog.js
+++ js/blog.js
@@ -2,7 +2,7 @@
     var textiie = document.querySelectorAll('.blog-inner .blog-scroll-section .load-more .blog-content .post-content');
     
    
-    var requestURL = 'http://www.fuelscale.com/json/blog.json';
+    var requestURL = 'json/blog.json';
     var request = new XMLHttpRequest();
     request.open('GET', requestURL);
     request.responseType = 'json';
@@ -13,11 +13,12 @@
       
     }
     function populateHeader(jsonObj) {
-      var bloge = jsonObj['blog'];  
-      for (i=0;i<100;i++){
+      var bloge = jsonObj['blog'];
+
+      for (i=0;i<bloge.length;i++){
         var myH1 = document.createElement('h1');
         var myP = document.createElement('p');
-        
+
         myH1.textContent= bloge[i].title;
         myP.textContent = bloge[i].content;
         section[i].appendChild(myH1);
